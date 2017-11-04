4a581edb703fcb74fb7cb259afd02d1fae627791
Thomas Schubert <thomas.schubert@42why.de>
2017-11-04 15:42:01 +0100

    Transform data for visualization

--- R/load-data.R
+++ R/load-data.R
@@ -41,7 +41,6 @@ hh <- hh_raw %>%
     key = date, 
     value = cnt, 
     -statistischer_Bezirk_Name, -statistischer_Bezirk_Nr, -file) %>% 
-  arrange(statistischer_Bezirk_Name) %>% 
   mutate(
     grp = case_when(
       file == "hh_priv"          ~ "sum",
@@ -51,11 +50,30 @@ hh <- hh_raw %>%
     year = str_sub(date, 7, 10)) %>% 
   rename(reg_name = statistischer_Bezirk_Name,
          reg_id   = statistischer_Bezirk_Nr) %>% 
-  select(reg_name, reg_id, grp, year, cnt) %>% 
+  select(year, reg_id, reg_name, grp, cnt) %>% 
   # 2017 is not yet available
-  filter(year != 2017)
+  filter(year != 2017) %>% 
+  arrange(reg_id, grp, year)
 
 
 
 # Transform data ----------------------------------------------------------
-hh
+hh_final <- hh %>% 
+  left_join(
+    hh %>% filter(grp == "sum"), 
+    by = c("year" = "year", "reg_id" = "reg_id")) %>% 
+  select(year, reg_id, reg_name.x, grp.x, cnt.x, cnt.y) %>% 
+  rename(
+    reg_name = reg_name.x,
+    grp = grp.x,
+    cnt = cnt.x,
+    cnt_sum = cnt.y) %>% 
+  mutate(
+    ratio = cnt/cnt_sum) %>% 
+  group_by(reg_id, grp) %>% 
+  mutate(
+    ratio_last_year = lag(ratio),
+    chg_ratio = (ratio - ratio_last_year)/ratio_last_year) %>% 
+  ungroup() %>% 
+  select(-ratio_last_year)
+
